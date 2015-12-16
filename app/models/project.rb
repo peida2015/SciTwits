a89dd340c15635c31d69e5cd9b033cd5124df495
App Academy Student <student@appacademy.io>
2015-12-16 08:34:10 -0800

    From 12/15

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
   validates :title, :description, :field_id, :owner_id, :significance, presence: true
   validates :significance, length: { maximum: 1000 }
+
   belongs_to :user
-  
+
 end
